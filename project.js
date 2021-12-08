ab9eb93fa05c28d19652402bb0cf22e3d877bbfa
Peter Marton <petermarton@Peters-MacBook-Pro.local>
2021-12-08 19:59:41 +0000

    initial commit

--- project.js
+++ project.js
@@ -5,51 +5,266 @@ happiness = 100;
 attack = 100;
 defense = 100;
 
-function eat(){
+function statusOption () {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "statusOptionP",
+        message: "Do you want to check the Full or just the Modified status? ",
+        choices: ["Full status", "Modified status"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.statusOptionP == "Full status"){
+        console.log("-------------------")
+        console.log(pet);
+        console.log("-------------------")
+        console.log(`Health: ${health}`);
+        console.log(`Happiness: ${happiness}`);
+        console.log(`Attack: ${attack}`);
+        console.log(`Defense: ${defense}`);
+        console.log("-------------------")
+      } else {
+        console.log("-------------------")
+        console.log(`Health: ${health}`);
+        console.log(`Happiness: ${happiness}`);
+        console.log(`Attack: ${attack}`);
+        console.log(`Defense: ${defense}`);
+        console.log("-------------------")
+      }
+    })
+    .then(() => gameLoop())
+}
+
+function eat () {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "eatP",
+        message: "What would you like to eat?",
+        choices: ["Apple", "Cake", "Big Mac", "Protein Bar"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.eatP == "Apple") {
+        console.log("-------------------")
+        console.log("Mmmm, this Apple is delicious.")
+        console.log("-------------------")
+        health += 10;
+        happiness += 10;
+        console.log(`Health: +10`);
+        console.log(`Happiness: +10`);
+        console.log("-------------------")
+      }
+      if (answer.eatP == "Cake") {
+        console.log("-------------------")
+        console.log("Mmmm, this Cake is delicious.")
+        console.log("-------------------")
+        health -= 10;
+        happiness += 20;
+        console.log(`Health: -10`);
+        console.log(`Happiness: +20`);
+        console.log("-------------------")
+      }
+      if (answer.eatP == "Big Mac") {
+        console.log("-------------------")
+        console.log("Mmmm, this Big Mac is delicious.")
+        console.log("-------------------")
+        health -= 30;
+        happiness += 30;
+        console.log(`Health: -30`);
+        console.log(`Happiness: +30`);
+        console.log("-------------------")
+      }
+      if (answer.eatP == "Protein Bar") {
+        console.log("-------------------")
+        console.log("Mmmm, this Protein Bar is delicious.")
+        console.log("-------------------")
+        health += 20;
+        happiness += 10;
+        console.log(`Health: +20`);
+        console.log(`Happiness: +10`);
+        console.log("-------------------")
+      }
+    })
+    .then(() => gameLoop())
+}
+
+
+function drink () {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "drinkP",
+        message: "What would you like to drink?",
+        choices: ["Tea", "Coffee", "Coke", "Magic Potion"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.drinkP == "Tea") {
+        console.log("-------------------")
+        console.log("Aaah, this Tea is delicious.")
+        console.log("-------------------")
+        health += 15;
+        happiness += 15;
+        console.log(`Health: +15`);
+        console.log(`Happiness: +15`);
+        console.log("-------------------")
+      }
+      if (answer.drinkP == "Coffee") {
+        console.log("-------------------")
+        console.log("Aaah, this Coffee is delicious.")
+        console.log("-------------------")
+        health -= 15;
+        happiness += 15;
+        console.log(`Health: -15`);
+        console.log(`Happiness: +15`);
+        console.log("-------------------")
+      }
+      if (answer.drinkP == "Coke") {
+        console.log("-------------------")
+        console.log("Aaah, this Coke is delicious.")
+        console.log("-------------------")
+        health -= 40;
+        happiness += 40;
+        console.log(`Health: -30`);
+        console.log(`Happiness: +40`);
+        console.log("-------------------")
+      }
+      if (answer.drinkP == "Magic Potion") {
+        console.log("-------------------")
+        console.log("Aaah, this Magic Potion is delicious.")
+        console.log("-------------------")
+        number1 = Math.floor(Math.random()*30);
+        number2 = Math.floor(Math.random()*20);
+        health += number1;
+        happiness += number2;
+        console.log(`Health: +${number1}`);
+        console.log(`Happiness: +${number2}`);
+        console.log("-------------------")
+      }
+    })
+    .then(() => gameLoop())
+}
+
+function play () {
   health = health +12;
   happiness = happiness +7;
 }
 
-function drink(){
+function hunt () {
   health = health +12;
   happiness = happiness +7;
 }
 
-function play(){
+function learn () {
   health = health +12;
   happiness = happiness +7;
 }
 
-function hunt(){
-  health = health +12;
-  happiness = happiness +7;
+function sleep () {
+
 }
 
-function learn(){
-  health = health +12;
-  happiness = happiness +7;
+function bath () {
+
 }
 
-
-const magic = () => {
-  health += 10;
-  happiness += 10;
-  attack += 10;
-  defense += 10;
-}
-
-const meditate = () => {
-  health += 35;
-  happiness += 35;
-  attack -= 10;
-  defense -= 35;
-}
-
-const bite = () => {
-  health -= 10;
-  happiness += 25;
-  attack += 10;
-  defense -= 10;
+function skills() {
+  const magic = () => {
+    num1 = Math.floor(Math.random()*20) - Math.floor(Math.random()*15);
+    num2 = Math.floor(Math.random()*20) - Math.floor(Math.random()*15);
+    num3= Math.floor(Math.random()*20) - Math.floor(Math.random()*15);
+    num4 = Math.floor(Math.random()*20) - Math.floor(Math.random()*15);
+    health += num1;
+    happiness += num2;
+    attack += num3;
+    defense += num4;
+  }
+  
+  const meditate = () => {
+    med1 = Math.floor(Math.random()*35);
+    med2 = Math.floor(Math.random()*35);
+    med3 = Math.floor(Math.random()*10);
+    med4 = Math.floor(Math.random()*35);
+    health += med1;
+    happiness += med2;
+    attack -= med3;
+    defense -= med4;
+  }
+  
+  const bite = () => {
+    health -= 10;
+    happiness += 25;
+    attack += 10;
+    defense -= 10;
+  }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "skillsP",
+        message: "Are you sure, that you want to use, your Unic skill? ",
+        choices: ["Yes", "No", "Maybe"],
+      },
+    ])
+    .then((answer) => {
+      if (answer.skillsP == "Yes") {
+        if (pet.skill == "Magic") {
+          console.log("-------------------")
+          console.log(">Magic< has been used!")
+          console.log("-------------------")
+          magic();
+          if (num1 < 0){
+            console.log(`Health: ${this.num1}`);
+          } else {
+            console.log(`Health: +${this.num1}`);
+          }
+          if (num2 < 0){
+            console.log(`Happiness: ${this.num2}`);
+          } else {
+            console.log(`Happiness: +${this.num2}`);
+          }
+          if (num3 < 0){
+            console.log(`Attack: ${this.num3}`);
+          } else {
+            console.log(`Attack: +${this.num3}`);
+          }
+          if (num4 < 0){
+            console.log(`Defense: ${this.num4}`);
+          } else {
+            console.log(`Defense: +${this.num4}`);
+          }
+          console.log("-------------------")
+        } else if (pet.skill == "Meditate") {
+          console.log("-------------------")
+          console.log(">Meditate< has been used!")
+          console.log("-------------------")
+          meditate();
+          console.log(`Health: ${this.med1}`);
+          console.log(`Happiness: ${thismed2}`);
+          console.log(`Attack: -10`);
+          console.log(`Defense: -35`);
+          console.log("-------------------")
+        } else {
+          console.log("-------------------")
+          console.log(">Bite< has been used!")
+          console.log("-------------------")
+          console.log(`Health: -10`);
+          console.log(`Happiness: +25`);
+          console.log(`Attack: +10`);
+          console.log(`Defense: -10`);
+          bite();
+          console.log("-------------------")
+        }
+      } else {
+        console.log("Interact with your pet :)")
+      }
+    })
+    .then(() => gameLoop())
 }
 
 class Pet {
@@ -316,80 +531,47 @@ const gameLoop = () => {
         type: "list",
         name: "optionP",
         message: "What would you like to do next? ",
-        choices: ["Check your Pet Status >", "Eat", "Drink", "Play", "Hunt", "Learn", "Use your Unic skill"],
+        choices: ["> Check your Pet Status <", "Eat", "Drink", "Play", "Hunt", "Learn", "Sleep", "Bath", "* Use your Unic skill *"],
       },
     ])
     .then((answer) => {
-      if (answer.optionP == "Check your Pet Status >"){
-        console.log("-------------------")
-        console.log(pet);
-        console.log("-------------------")
-        console.log(`Health: ${health}`);
-        console.log(`Happiness: ${happiness}`);
-        console.log(`Attack: ${attack}`);
-        console.log(`Defense: ${defense}`);
-        console.log("-------------------")
+      if (answer.optionP == "> Check your Pet Status <"){
+        console.log("-------------------");
+        statusOption();
       }
       if (answer.optionP == "Eat"){
-        console.log("-------------------")
+        console.log("-------------------");
         eat();
-        console.log("-------------------")
       }
       if (answer.optionP == "Drink"){
-        console.log("-------------------")
+        console.log("-------------------");
         drink();
-        console.log("-------------------")
       }
       if (answer.optionP == "Play"){
-        console.log("-------------------")
+        console.log("-------------------");
         play();
-        console.log("-------------------")
       }
       if (answer.optionP == "Hunt"){
-        console.log("-------------------")
+        console.log("-------------------");
         hunt();
-        console.log("-------------------")
       }
       if (answer.optionP == "Learn"){
-        console.log("-------------------")
+        console.log("-------------------");
         learn();
-        console.log("-------------------")
       }
-      if (answer.optionP == "Use your Unic skill"){
-        if (pet.skill == "Magic") {
-          console.log("-------------------")
-          console.log(">Magic< has been used!")
-          console.log("-------------------")
-          console.log(`Health: +10`);
-          console.log(`Happiness: +10`);
-          console.log(`Attack: +10`);
-          console.log(`Defense: +10`);
-          magic();
-          console.log("-------------------")
-        } else if (pet.skill == "Meditate") {
-          console.log("-------------------")
-          console.log(">Meditate< has been used!")
-          console.log("-------------------")
-          console.log(`Health: +35`);
-          console.log(`Happiness: +35`);
-          console.log(`Attack: -10`);
-          console.log(`Defense: -35`);
-          meditate();
-          console.log("-------------------")
-        } else {
-          console.log("-------------------")
-          console.log(">Bite< has been used!")
-          console.log("-------------------")
-          console.log(`Health: -10`);
-          console.log(`Happiness: +25`);
-          console.log(`Attack: +10`);
-          console.log(`Defense: -10`);
-          bite();
-          console.log("-------------------")
-        }
+      if (answer.optionP == "Sleep"){
+        console.log("-------------------");
+        sleep();
+      }
+      if (answer.optionP == "Bath"){
+        console.log("-------------------");
+        bath();
+      }
+      if (answer.optionP == "* Use your Unic skill *"){
+        console.log("-------------------");
+        skills();
       }
     })
-    .then(() => gameLoop())
 }
 
 init();
